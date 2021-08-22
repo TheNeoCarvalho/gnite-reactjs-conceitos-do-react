484ced75add27f4ee33532ef2e74df6148a3f8a1
Manoel Carvalho <manoel.carvalho@mediacaonline.com>
2021-08-21 22:40:27 -0300

    Finish

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from 'react-icons/fi';
 
 interface Task {
   id: number;
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return
+    }
+
+    const id = Math.floor(Math.random() * 1000)
+
+    const newTask: Task[] = {
+      id,
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const Update = tasks.map(task => task.id === id ? {...task, isComplete: !task.isComplete}: task);
+    setTasks(Update);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const delTasks = tasks.filter(task => task.id !== id);
+    setTasks(delTasks);
+
   }
 
   return (
